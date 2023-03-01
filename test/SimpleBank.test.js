9af5335dee08701dda4cae8451c80eea886fad03
Ivan <datskoivan19@gmail.com>
2023-03-01 19:52:47 +0200

    ya ustal....
    added 1.5 tests....

--- test/SimpleBank.test.js
+++ test/SimpleBank.test.js
@@ -8,16 +8,42 @@ describe("SimpleBank", () => {
     let acc3 
     let simpleBank
 
+    let isMember
+
     beforeEach(async () => {
         [owner, acc1, acc2, acc3] = await ethers.getSigners()
         const SimpleBank = await ethers.getContractFactory("SimpleBank", owner)
         simpleBank = await SimpleBank.deploy()
         await simpleBank.deployed()
-        console.log(simpleBank.address)
         expect(simpleBank.address).to.be.properAddress
+        isMember = await simpleBank.isMember;
     })
 
-    it("should be deployed", async () => {
-        console.log("success!")
+    it("should have 0 values by default", async () => {
+        const balance = await simpleBank.balance()
+        expect(balance).to.eq(0)
+        
+        const sum = await simpleBank.sum()
+        expect(sum).to.eq(0)
+
+        const bankState = await simpleBank.currentBankState()
+        expect(bankState).to.be.false
+
+        const members = await simpleBank.members
+        expect(members.length).to.eq(0)
     })
+
+    it("should set sum and add owner as member on start", async () => {
+        const sum = 100;
+        await simpleBank.start(sum);
+        
+        const isOwnerMember = await isMember[owner.address]
+        expect(isOwnerMember).to.be.true;
+      
+        const bankState = await simpleBank.currentBankState();
+        expect(bankState).to.be.true;
+      
+        const contractSum = await simpleBank.sum();
+        expect(contractSum).to.equal(sum);
+      });
 })
