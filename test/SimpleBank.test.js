07422dc8b16f19ef981ce4858c6ae18c77d240f5
Ivan <datskoivan19@gmail.com>
2023-03-02 10:57:10 +0200

    add a few tests

--- test/SimpleBank.test.js
+++ test/SimpleBank.test.js
@@ -8,7 +8,6 @@ describe("SimpleBank", () => {
     let acc3 
     let simpleBank
 
-    let isMember
 
     beforeEach(async () => {
         [owner, acc1, acc2, acc3] = await ethers.getSigners()
@@ -16,7 +15,6 @@ describe("SimpleBank", () => {
         simpleBank = await SimpleBank.deploy()
         await simpleBank.deployed()
         expect(simpleBank.address).to.be.properAddress
-        isMember = await simpleBank.isMember;
     })
 
     it("should have 0 values by default", async () => {
@@ -34,16 +32,30 @@ describe("SimpleBank", () => {
     })
 
     it("should set sum and add owner as member on start", async () => {
-        const sum = 100;
-        await simpleBank.start(sum);
+        const sum = 100
+        await simpleBank.start(sum)
         
-        const isOwnerMember = await isMember[owner.address]
-        expect(isOwnerMember).to.be.true;
+        const isOwnerMember = await simpleBank.isAccMember(owner.address)
+        expect(isOwnerMember).to.be.true
       
-        const bankState = await simpleBank.currentBankState();
-        expect(bankState).to.be.true;
+        const bankState = await simpleBank.currentBankState()
+        expect(bankState).to.be.true
       
-        const contractSum = await simpleBank.sum();
-        expect(contractSum).to.equal(sum);
+        const contractSum = await simpleBank.sum()
+        expect(contractSum).to.eq(sum)
       });
+
+
+    it("should add new member to mapping and array with function addMember", async () => {
+        const sum = 100
+        await simpleBank.start(sum)
+
+        await simpleBank.addMember(acc1.address)
+
+        const isUserMember = await simpleBank.isAccMember(acc1.address)
+        expect(isUserMember).to.be.true
+
+        const member = await simpleBank.members(1)
+        expect(member).to.eq(acc1.address)
+    })
 })
